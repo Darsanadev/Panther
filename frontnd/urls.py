836aaa64144f2847aff7b6aec6daba7ffbc5b6d2
Darsanadev <darsana8123@gmail.com>
2024-09-30 15:45:43 +0530

    first commit

---
+++ frontnd/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'frontnd'
+
+urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('index/', views.home, name='index'),
+    path('contact/', views.contact, name='contact'),
+]
