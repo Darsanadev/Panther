39782603111cac5418e004ff4c459b4135cf6567
Darsanadev <darsana8123@gmail.com>
2025-01-11 14:19:50 +0000

    commit from server

--- frontnd/urls.py
+++ frontnd/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'frontnd'
 
 urlpatterns = [
+
     path('about/', views.about, name='about'),
     path('', views.home, name='index'),
     path('contact/', views.contact, name='contact'),
